0961c8827c52d12ffc82929b418e902f630df78b
Nabakumr <nabakumarbanerjee70@gmail.com>
2025-09-22 13:24:59 +0530

    Fix

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ for rule in app.url_map.iter_rules():
     print(f"{rule} -> {rule.endpoint}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
