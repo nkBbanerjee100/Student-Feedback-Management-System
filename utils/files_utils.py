611d62c218c3c9c1e1feae7b625c1b73eefa471c
Nabakumr <nabakumarbanerjee70@gmail.com>
2025-09-24 10:25:54 +0530

    check1

--- utils/files_utils.py
+++ utils/files_utils.py
@@ -6,3 +6,8 @@ class FUtils:
         logs_dir = os.path.join(current_app.root_path, "logs")
         os.makedirs(logs_dir, exist_ok=True)
         return os.path.join(logs_dir, "app.log")
+
+
+# config.py
+# import os
+# DB_URL = os.getenv("DATABASE_URL", "mysql+pymysql://python_user:MyPassw0rd%21@localhost:3306/mydb")
